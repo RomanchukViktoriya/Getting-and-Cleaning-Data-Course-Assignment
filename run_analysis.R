1c7d4f3fb5d5e7a60f7251f598872a1066ddccb9
RomanchukViktoriya <RomanchukViktoriya@users.noreply.github.com>
2015-01-22 12:43:33 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -72,4 +72,5 @@ melt_data      = melt(data_new, id = id_labels, measure.vars = data_labels)
 # Applying mean function to dataset using dcast function
 tidy_data   = dcast(melt_data, subject + Activity_Label ~ variable, mean)
 
-write.table(tidy_data, file = "data/tidy_data.txt")
+write.table(tidy_data, file = "./tidy_data.txt", row.name=FALSE)
+
