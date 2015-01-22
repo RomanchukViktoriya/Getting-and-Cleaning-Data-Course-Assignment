2bd3d99554adfc63aefbb4421eb5a1332bfcf5ac
RomanchukViktoriya <RomanchukViktoriya@users.noreply.github.com>
2015-01-22 12:38:39 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -15,39 +15,19 @@ if (!require("reshape2")) {
 require("data.table")
 require("reshape2")
 
-library(plyr)
-
-## We are downloading our data
-
-download.data = function() {
-    "Checks for data directory and creates one if it doesn't exist"
-    if (!file.exists("data")) {
-        message("Creating data directory")
-        dir.create("data")
-    }
-    if (!file.exists("data/UCI HAR Dataset")) {
-        # download the data
-        fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-        zipfile="data/UCI_HAR_data.zip"
-        message("Downloading data")
-        download.file(fileURL, destfile=zipfile, method="curl")
-        unzip(zipfile, exdir="data")
-    }
-}
-
 
 # Loading data for activity labels
-activity_labels <- read.table("data/UCI HAR Dataset/activity_labels.txt")[,2]
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")[,2]
 
 # Loading data for data column names
-features <- read.table("data/UCI HAR Dataset/features.txt")[,2]
+features <- read.table("./UCI HAR Dataset/features.txt")[,2]
 
 # Extracting only the measurements on the mean and standard deviation for each measurement.
 extract_features <- grepl("mean|std", features)
 
 # Loading and processing data for X_test & Y_test
-X_test <- read.table("data/UCI HAR Dataset/test/X_test.txt")
-Y_test <- read.table("data/UCI HAR Dataset/test/y_test.txt")
+X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
+Y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
 subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 
 names(X_test) = features
@@ -64,10 +44,10 @@ names(subject_test) = "subject"
 test_data <- cbind(as.data.table(subject_test), Y_test, X_test)
 
 # Loading and processing data for X_train & Y_train.
-X_train <- read.table("data/UCI HAR Dataset/train/X_train.txt")
-Y_train <- read.table("data/UCI HAR Dataset/train/y_train.txt")
+X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
+Y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
 
-subject_train <- read.table("data/UCI HAR Dataset/train/subject_train.txt")
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 
 names(X_train) = features
 
@@ -83,11 +63,11 @@ names(subject_train) = "subject"
 train_data <- cbind(as.data.table(subject_train), Y_train, X_train)
 
 # Merging test and train data
-data = rbind(test_data, train_data)
+data_new = rbind(test_data, train_data)
 
 id_labels   = c("subject", "Activity_ID", "Activity_Label")
-data_labels = setdiff(colnames(data), id_labels)
-melt_data      = melt(data, id = id_labels, measure.vars = data_labels)
+data_labels = setdiff(colnames(data_new), id_labels)
+melt_data      = melt(data_new, id = id_labels, measure.vars = data_labels)
 
 # Applying mean function to dataset using dcast function
 tidy_data   = dcast(melt_data, subject + Activity_Label ~ variable, mean)
